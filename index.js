0221df84133483e269e01d6035717b6397d27ee3
AndrewMedvedev42 <sanjino56@gmail.com>
2023-09-20 11:07:10 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const filterList = document.querySelector('#filters')
+filterList.innerHTML += `
+    <select name="" id="">
+        <option value="">Option 1</option>
+        <option value="">Option 2</option>
+        <option value="">Option 3</option>
+    </select>
+`
